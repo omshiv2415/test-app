be9cc7b9e0249aa4df32407b8ecf64389ddb36b2
omshiv2415 <omshiv2415@yahoo.co.uk>
2016-01-15 23:16:41 +0000

    admin added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -75,7 +75,7 @@ class ArticlesController < ApplicationController
     end
 
 	  def require_same_user
-       if current_user != @article.user or !current_user.admin?
+			if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only edit or delete your own articles"
         redirect_to root_path
 			end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:edit, :update, :show]
-before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+	before_action :require_admin, only: [:destroy]
 def index
 @users = User.paginate(page: params[:page], per_page: 5)
 end
@@ -30,6 +31,19 @@ end
 def show
 @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 end
+
+	def destroy
+		@user_articles =  User.find(params[:id])
+		@user.destroy
+		flash[:danger] = "User and all articles created by user have been deleted"
+		reditect_to user_path
+	end
+	def require_admin
+		if logged_in? and !current_user.admin?
+			flash[:danger]= "only admin can perform this action"
+			redirect_to root_path
+	end
+	end
 private
 def user_params
 params.require(:user).permit(:username, :email, :password)
@@ -38,7 +52,7 @@ def set_user
 @user = User.find(params[:id])
 end
 	def  require_same_user
-		if current_user != @user
+		if current_user != @user and !current_user.admin?
 				flash[:danger] ="You can only edit your own account"
 			redirect_to users_path
 			end
