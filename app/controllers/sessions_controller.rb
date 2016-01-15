8f9df05e8a394025ea1613b66e34be4d3eb7415f
omshiv2415 <omshiv2415@yahoo.co.uk>
2016-01-15 21:18:54 +0000

    windows

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,8 @@ def new
 
 end
 
-
 def create
-	user = User.find_by(email: params[:session][:email].downcase)
+user = User.find_by(email: params[:session][:email].downcase)
 if user && user.authenticate(params[:session][:password])
 session[:user_id] = user.id
 flash[:success] = "You have successfully logged in"
@@ -15,10 +14,6 @@ flash.now[:danger] = "There was something wrong with your login information"
 render 'new'
 end
 end
-
-
-
-
 
 def destroy
 session[:user_id] = nil
