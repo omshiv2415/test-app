d0eed02341b6e2bcea59f24fca484a00b5c2f899
omshiv2415 <omshiv2415@yahoo.co.uk>
2016-01-16 11:05:06 +0000

    testing functionality added

---
+++ test/models/category_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+class CategoryTest < ActiveSupport::TestCase
+	def setup
+		@category = Category.new(name: "sports")
+	end
+	test "category shoul be valid" do
+		assert @category.valid?
+	end
+
+	test "name should be present" do
+		@category.name = ""
+		assert_not @category.valid?
+	end
+
+	test "name shoub be unique" do
+		@category.save
+		category2 = Category.new(name: "sports")
+		assert_not category2.valid?
+	end
+	test "name should not be to loong" do
+		@category.name = "a"  * 26
+		assert_not @category.valid?
+	end
+	test "name should not be to short" do
+		@category.name = "aa"
+		assert_not @category.valid?
+	end
+
+
+end
